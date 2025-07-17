675ac41bf750201899cd43299442c204cd48781e
abdullah-mamun369 <maamamun369@gmail.com>
2025-07-17 14:40:12 +0600

    Project setup with redux, MUI, axios done

---
+++ src/services/auth.services.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { authKey } from "@/constant/authkey";
+import { decodedToken } from "@/utils/jwt";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
+
+export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+  return setToLocalStorage(authKey, accessToken);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage(authKey);
+
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      //   role: decodedData?.role.toLwerCase(),
+    };
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+
+  if (authToken) {
+    return !!authToken;
+  }
+};
+
+export const removeUser = () => {
+  return removeFromLocalStorage(authKey);
+};
